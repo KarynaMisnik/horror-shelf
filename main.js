72c1b5465c788b43cb125d87a87f5faef5cd5c57
KarynaMisnik <karinamisnik94@gmail.com>
2025-07-01 13:10:00 +0300

    form function added

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const form = document.getElementById("add-movie-form");
 const movieList = document.getElementById("movie-list");
 
-form.addEventListener("submit", function (e) {});
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent the form from submitting normally
+  const titleInput = document.getElementById("title");
+  const title = titleInput.ariaValueMax.trim(); // Get the value of the title input and trim whitespace
+});
