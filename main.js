0da19d34021ba5f8489b0a1dbe86c06dd1f55ae1
KarynaMisnik <karinamisnik94@gmail.com>
2025-07-03 13:05:00 +0300

    movie form inputs added

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ form.addEventListener("submit", function (e) {
   e.preventDefault(); // Prevent the form from submitting normally
   const titleInput = document.getElementById("title");
   const title = titleInput.value.trim(); // Get the value of the title input and trim whitespace
+  const descriptionInput = document.getElementById("description");
+  const description = descriptionInput.value;
 
   if (!title) {
     error.textContent = "Title cannot be empty";
@@ -16,9 +18,13 @@ form.addEventListener("submit", function (e) {
 
   // Add movie
   const p = document.createElement("p");
+  const text_description = document.createElement("p");
   p.textContent = title;
+  text_description.textContent = description;
   movieList.appendChild(p);
+  movieList.appendChild(text_description);
 
   // Clear input
   titleInput.value = "";
+  descriptionInput.value = "";
 });
